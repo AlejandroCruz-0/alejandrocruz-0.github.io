bb9471f8cd6bc308c74b36b2e98c65906ab9c78d
Alejandro Cruz <alejandro.cruz.jeria@gmail.com>
2023-12-04 03:04:31 +0000

    Added google analytics
    Added partytown library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://alejandrocruz-0.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(),
+		partytown({
+			config: {
+			forward: ["dataLayer.push"],
+			},
+		}),],
 });
